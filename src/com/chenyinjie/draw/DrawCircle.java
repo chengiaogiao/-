1268dfe4b616862bddc16bacf320cd827da01c0a
Charlie.chen <charlie.chen@elitesland.com>
2026-09-09 18:03:22 +0800

    first commit

---
+++ src/com/chenyinjie/draw/DrawCircle.java
@@ -0,0 +1,58 @@
+package com.chenyinjie.draw;
+
+import javafx.scene.layout.Pane;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawCircle extends JFrame {
+    private MyPanel a=null;
+    public static void main(String[] args) {
+       new DrawCircle();
+    }
+
+   public DrawCircle(){
+       a = new MyPanel();
+       this.add(a);
+       this.setSize(300,400);
+       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       this.setVisible(true);
+   }
+}
+
+class MyPanel extends JPanel{
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+//        g.drawOval(100,200,100,100);
+//        g.drawLine(10,20,20,30);
+//        g.drawRect(10,10,20,20);
+//
+//        g.setColor(Color.BLACK);
+//        g.fillRect(20,20,30,30);
+//
+//        g.setColor(Color.BLACK);
+//        g.fillOval(30,30,40,40);
+
+//        Image image=Toolkit.getDefaultToolkit().getImage(Panel.class.getResource("/bg.jpg"));
+//        g.drawImage(image,10,20,100,100,this);
+//
+//
+//        g.setColor(Color.black);
+//
+//        g.setFont(new Font("草书",Font.BOLD,50));
+//
+//        g.drawString("陈英杰",200,100);
+        g.setColor(Color.blue);
+        g.fillRect(30,50,10,50);
+        g.setColor(Color.blue);
+        g.fillRect(60,50,10,50);
+        g.setColor(Color.blue);
+        g.fillRect(40,60,30,30);
+        g.setColor(Color.red);
+        g.fillOval(42,67,15,15);
+        g.setColor(Color.green);
+        g.fillRect(47,47,5,30);
+    }
+}
